19d6f834df8f76af21d5667725a4838fa78fced6
Mohamed Elashri <s-mohamed.elashri@zewailcity.edu.eg>
2021-10-07 03:40:36 -0400

    fix typo while correcting things

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # loop over all notebooks and run nbconvert over them
 for i in $(find . | grep -F .ipynb); do
-  jupyter nbconvert --to markdown $i
+  jupyter nbconvert --to pdf $i
   echo $i
 done
 
